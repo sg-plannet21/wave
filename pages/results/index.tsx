f88c01e61b65a0b173ddf30a83909857f3c11924
Stephen Gallagher <stephen.gallagher@plannet21.ie>
2022-10-26 10:55:12 +0100

    fix: removed former PrimaryLayout prop

--- pages/results/index.tsx
+++ pages/results/index.tsx
@@ -62,5 +62,5 @@ const Results: NextPageWithLayout<IResults> = ({ searchResults }) => {
 export default Results;
 
 Results.getLayout = (page) => {
-  return <PrimaryLayout justify="items-start">{page}</PrimaryLayout>;
+  return <PrimaryLayout>{page}</PrimaryLayout>;
 };
