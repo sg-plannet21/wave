b82ec3799cc95c9fe057c870ea2d649c5b83e457
Stephen Gallagher <stephen.gallagher@plannet21.ie>
2022-12-04 20:43:30 +0000

    feat: added useRoutesTableData hook

--- features/pages/routes/hooks/useRoutesTableData.ts
+++ features/pages/routes/hooks/useRoutesTableData.ts
@@ -1,31 +1,72 @@
 import _ from 'lodash';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import useWaveCollectionRequest from 'state/hooks/useWaveCollectionRequest';
-import { Route, RouteDestinationType, RouteTableEntity } from '../types';
+import { Route, RouteDestinationType } from '../types';
+
+export type RouteTableRecord = {
+  id: string;
+  name: string;
+  destinationTypeLabel: string;
+  system: boolean;
+  systemCreatedLabel: string;
+};
 
 export function useRouteTableData() {
+  const [showSystemRoutes, setShowSystemRoutes] = useState<boolean>(false);
+  const [destinationExceptionList, setDestinationExceptionList] = useState<
+    string[]
+  >([]);
+
   const { data: routes, error: routesError } =
     useWaveCollectionRequest<Route>('routes');
   const { data: destinationTypes, error: destinationTypesError } =
     useWaveCollectionRequest<RouteDestinationType>('routeDestinationTypes');
 
-  const routeTableData = useMemo((): RouteTableEntity[] => {
+  const data: RouteTableRecord[] = useMemo(() => {
     if (!routes || !destinationTypes) return [];
 
     return Object.values(routes).map((route) => ({
-      ...route,
-      system_created_label: route.system_created ? 'System' : 'Custom',
-      destination_type_label: _.get(
+      id: route.route_id,
+      name: route.route_name,
+      system: route.system_created,
+      systemCreatedLabel: route.system_created ? 'System' : 'Custom',
+      destinationTypeLabel: _.get(
         destinationTypes[route.destination_type],
         'destination_type'
       ),
     }));
   }, [routes, destinationTypes]);
 
+  const filteredBySystem = useMemo(() => {
+    return showSystemRoutes ? data : data.filter((route) => !route.system);
+  }, [data, showSystemRoutes]);
+
+  const filteredByDestination = useMemo(() => {
+    return filteredBySystem.filter(
+      (route) => !destinationExceptionList.includes(route.destinationTypeLabel)
+    );
+  }, [filteredBySystem, destinationExceptionList]);
+
+  const destinationList: string[] = useMemo(() => {
+    return _.chain(filteredBySystem)
+      .map((route) => route.destinationTypeLabel)
+      .filter((routeLabel) => !!routeLabel)
+      .sortBy((route) => route.toLowerCase())
+      .uniq()
+      .value();
+  }, [filteredBySystem]);
+
   return {
     isLoading:
       !routes && !destinationTypes && !routesError && !destinationTypesError,
-    data: routeTableData,
     error: routesError || destinationTypesError,
+    data: filteredByDestination,
+    filters: {
+      isSystemRoutes: showSystemRoutes,
+      setSystemRoutes: setShowSystemRoutes,
+      destinationList,
+      destinationExceptionList,
+      setDestinationExceptionList,
+    },
   };
 }
